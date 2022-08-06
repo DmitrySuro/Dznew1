64ef39d1549880ac79d8bbc95637aba59d47a149
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-08-06 21:53:42 +0300

    addTask6

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 6: Напишите программу, которая на вход 
+//принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+System.Console.Clear
+
+System.Console.WriteLine("ghf");
+int number = int.Parse(System.Console.ReadLine()!);
+
+if (number%2 == 0) System.Console.WriteLine($"Ваше число четное - {number}");
+if (number%2 == 1) System.Console.WriteLine($"Ваше число нечетное - {number}");
